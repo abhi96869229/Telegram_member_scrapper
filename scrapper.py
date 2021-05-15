b3c04541854cb0929a570f31d2d3eab01ad37e8e
mayvidxd <73879481+mayvidxd@users.noreply.github.com>
2021-05-15 14:00:19 +0530

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,54 @@
+import os
+import time
+from telethon.sync import TelegramClient
+from telethon import TelegramClient,events,sync
+from telethon.sessions import StringSession
+from telethon import functions,types
+from telethon.sync import TelegramClient
+from telethon import functions, types
+
+api_id = os.environ.get("API_ID")
+api_hash = os.environ.get("API_HASH")
+owner = os.environ.get("USER_ID")
+owner = int(owner)
+string_session = os.environ.get("STRING_SESSION")
+chan = os.environ.get("GROUP1")
+chann = os.environ.get("GROUP2")
+client = TelegramClient(StringSession(string_session), api_id, api_hash)
+client.start()
+usersscraped = list()
+cm = 0
+for user in client.iter_participants(chan):
+    try:
+        userinfo = client(functions.users.GetFullUserRequest(
+            id=user.username
+        ))
+        try:
+            
+            result = client(functions.contacts.AddContactRequest(
+                id=user.username,
+                first_name=userinfo.user.first_name,
+                last_name=userinfo.user.last_name,
+                phone = "+91 ",
+                add_phone_privacy_exception=False
+            ))
+            try:
+                scrap = client(functions.channels.InviteToChannelRequest(
+                    channel=chann,
+                    users=[user.username]
+                ))
+                usersscraped.append(user.username)
+                print("useradded")
+                cm = cm+1
+                print(cm)
+                if cm%15 == 0:
+                    time.sleep(90000)
+                    client.send_message(1476503457,"BOT STARTED.")
+            except:
+                 pass
+        except:
+            pass
+    except:
+        pass
+    
+
